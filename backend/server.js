dc078b29ec69fadcf0b3edf83d0df5cfbf4f728a
amashou <amano.criaju@gmail.com>
2022-04-17 10:59:21 +0900

    修正

--- backend/server.js
+++ backend/server.js
@@ -9,18 +9,18 @@ const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established suuccessfully");
-});
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established suuccessfully");
+// });
 
-const exercisesRouter = require("./routes/exercises");
-const usersRotuer = require("./routes/users");
+// const exercisesRouter = require("./routes/exercises");
+// const usersRotuer = require("./routes/users");
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRotuer);
+// app.use("/exercises", exercisesRouter);
+// app.use("/users", usersRotuer);
 app.get('/', (req, res) => {
   console.log('get first requirest')
   res.send('success request to AWS EC2!')
